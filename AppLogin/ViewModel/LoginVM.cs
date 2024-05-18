6ab23d58f0f9c02118db6a63d639d7f04e98da22
Daniel-Venegas <167117774+Daniel-Venegas@users.noreply.github.com>
2024-05-18 09:44:16 -0500

    Delete AppLogin directory

--- AppLogin/ViewModel/LoginVM.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace AppLogin.ViewModel
-{
-    public class LoginVM
-    {
-
-        public required string Contacto { get; set; }
-        public required string password { get; set; }
-    }
-}
